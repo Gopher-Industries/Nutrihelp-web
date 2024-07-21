3e1f11777091b03848c052c583f9367c74d8caaf
blaz2f <46502527+blaz2f@users.noreply.github.com>
2024-07-21 21:28:06 +1000

    deprecating direct connection to supabase

--- src/routes/SearchRecipes/fetchRecipes.js
+++ src/routes/SearchRecipes/fetchRecipes.js
@@ -1,12 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
-let supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-let supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-let supabase = createClient(supabaseUrl, supabaseKey);
+//Please refactor it to using recipe API
+//let supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+//let supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+//let supabase = createClient(supabaseUrl, supabaseKey);
+
 
 
 export async function fetchRecipes() {
-    try {
+  /*  try {
         let { data, error } = await supabase
             .from('recipes')
             .select('*');
@@ -18,5 +20,46 @@ export async function fetchRecipes() {
         return data;
     } catch (error) {
         console.log(error)
-    }
+    }*/
+
+        return  {
+            "recipes": [
+              {
+                "id": 0,
+                "created_at": "string",
+                "recipe_name": "string",
+                "cuisine_id": 0,
+                "total_servings": 0,
+                "preparation_time": 0,
+                "ingredients": {
+                  "id": [
+                    0
+                  ],
+                  "quantity": [
+                    0
+                  ],
+                  "category": [
+                    "string"
+                  ],
+                  "name": [
+                    "string"
+                  ]
+                },
+                "instructions": "string",
+                "calories": 0,
+                "fat": 0,
+                "carbohydrates": 0,
+                "protein": 0,
+                "fiber": 0,
+                "vitamin_a": 0,
+                "vitamin_b": 0,
+                "vitamin_c": 0,
+                "vitamin_d": 0,
+                "sodium": 0,
+                "sugar": 0,
+                "cuisine_name": "string"
+              }
+            ]
+          }
+   
 }
