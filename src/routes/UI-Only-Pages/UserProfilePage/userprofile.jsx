6c187b74b3d70250ac51cab9347926c54e102512
Mihirotron <mihirjr2002@gmail.com>
2024-08-30 17:24:38 +1000

    The new enhacement of User Profile Page

--- src/routes/UI-Only-Pages/UserProfilePage/userprofile.jsx
+++ src/routes/UI-Only-Pages/UserProfilePage/userprofile.jsx
@@ -11,11 +11,13 @@ const UserProfilePage = () => {
   const handleInputValue = (e) => {
     e.preventDefault();
     const userFirstName = firstName.value;
+
     const userLastName = lastName.value;
     const userNumber = number.value;
     const userEmail = email.value;
     const userPassword = password.value;
     const userHealth = health.value;
+
     const userPreferred = preferred.value;
     const inputArray = [
       {
@@ -38,93 +40,129 @@ const UserProfilePage = () => {
   };
 
   return (
-    <div>
-      <div className="userProfile-container">
-        <div className="userProfile-section">
-          <div className="userProfile-content">
-            <div>
-              <div className="user-img-container">
-                <FaUserCircle size="150px" color="rgba(18, 18, 255, 0.701)" />
-              </div>
-              <p className="user-choose-photo-text">
-                <FaUserPen size="25px" /> Choose avatar
-              </p>
+    <div className="container-fluid bg-white">
+      <h2 className="text-center mycolor"> Register with us</h2>
+      <div className="container m-1">
+    <div className="custom-row w-100">
+        <div className="container70 w-70">
+        <form className="was-validated">
+        <div className="mb-3">
+    <label for="name" className="form-label">Full Name</label>
+    <input className="form-control is-invalid" id="name" placeholder="Type Your Name Here.." required/>
+    <span className="error">Valid Name </span>
+  </div>
 
-              <div className="user-bottom-text">
-                <div className="user-bottom-text-pair">
-                  <p>
-                    <AiFillBug /> Report a bug
-                  </p>
-                  <p>Something wrong? Let us know</p>
-                </div>
-                <div className="user-bottom-text-pair">
-                  <p>
-                    <MdDynamicFeed /> Feedback
-                  </p>
-                  <p>Have any suggestions? </p>
-                </div>
-              </div>
-            </div>
-            {/* form */}
-            <div className="user-form-container">
-              <form>
-                <div className="user-form-content">
-                  <label htmlFor="firstName">First Name</label>
-                  <input type="text" name="firstName" id="firstName" />
-                </div>
-                
-                <div className="user-form-content">
-                  <label htmlFor="lastName">Last Name</label>
-                  <input type="text" name="lastName" id="lastName" />
-                </div>
-                <div className="user-form-content">
-                  <label htmlFor="number">Number</label>
-                  <input type="text" name="number" id="number" />
-                </div>
-                <div className="user-form-content">
-                  <label htmlFor="email">Email</label>
-                  <input type="email" name="email" id="email" />
-                </div>
-                
-                <div className="user-form-content">
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </div>
-                <div className="user-form-content">
-                <label htmlFor="mfa">MFA</label>
-                  <div className="user-mfa-toggle">
-                  <label class="user-switch">
-                    <input
-                      type="checkbox"
-                      id="mfa"
-                      checked={isMFAEnabled}
-                      onChange={toggleMFA}
-                    />
-                    <span class="slider round"></span>
-                    </label>
-                  </div>
-                </div>
 
-                <div className="user-logout-btn-content">
-                  <button type="button" className="user-logout-btn">
-                    LogOut
-                  </button>
-                </div>
-              </form>
+  <div className="mb-3">
+    <label for="email" className="form-label">Email Id</label>
+    <input className="form-control is-invalid" id="email" placeholder="Type Your Email Here.." required/>
+    <span className="error">✗</span>
+  </div>
+
+  <div className="mb-3">
+    <label for="contact" className="form-label">Contact No</label>
+    <input className="form-control is-invalid" id="contact" placeholder="Type Your Phone No Here.." required/>
+    <span className="error">✗</span>
+    <div className="invalidfeedback"> Phone no is invalid</div>
+  </div>
+  <div className="mb-3">
+    <label for="password" className="form-label">Password</label>
+    <input className="form-control is-invalid"  id="password" type="password" required/>
+    <span className="error">✗</span>
+  </div>
+
+  <div className="form-group">
+            <label>Choose Gender</label>
+            <div className="gender-options">
+              <label>
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  name="gender"
+                  value="male"
+                />
+                Male
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  className="form-check-input"
+                  value="female"
+                />
+                Female
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  className="form-check-input"
+                  value="other"
+                />
+                Other
+              </label>
             </div>
           </div>
+          <div className="form-group">
+            <label for="food" className="formlabel">Food Preference</label>
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  name="foodPreference"
+                  value="veg"
+                />
+                <img src="./images/veg.png" className="foodimg"/><br/><br/>
+                <label>Veg</label>
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  name="foodPreference"
+                  value="vegan"
+                />
+                <img src="./images/vegan.png" className="foodimg"/><br/><br/>
+                <label>Vegan</label>
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  name="foodPreference"
+                  value="eggetarian"
+                />
+                <img src="./images/egg.png" className="foodimg"/><br/><br/>
+                <label>Eggetarian</label>
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  name="foodPreference"
+                  value="non-veg"
+                />
+               <img src="./images/nonveg.png" className="foodimg"/><br/><br/>
+               <label>Non Veg</label>
 
-          <div className="user-logout-btn-content">
-            <button className="user-logout-btn">Save</button>
-            <button className="user-logout-btn">Clear</button>
           </div>
+          <div className="form-group">
+            <label className="checkbox">
+              <input type="checkbox" name="signup" className="form-check-input"/>
+              By Signing up you agree to receive updates and special offers
+            </label>
+          </div>
+          <button type="submit" className="btn buttoncolor">SUBMIT</button>
+          <button type="reset" className="btn buttoncolor">RESET</button>
+</form>
+<div className="progress-bar">
+          <div className="progress"></div>
         </div>
-      </div>
+        </div>
+        <div className="container30 w-30">
+          <img src='./images/main.png' className="img"/>
+          <input type="file" className="form-control" name="image"/>
+        <div className="signup-options mt-5">
+          <button className="signup-google mt-5"><img src="/images/g1.png" class="signupimg me-2"/> Sign Up with Google</button>
+          <button className="signup-facebook mt-5"><img src="/images/f1.png" class="signupimg me-2"/> Sign Up with Facebook</button>
+          <button className="signup-help mt-5"><img src="/images/h.png" class="signupimg me-2"/> Need Help? Talk to Us.</button>
+        </div>
+        </div>
+    </div>
+    </div>
     </div>
   );
 };
