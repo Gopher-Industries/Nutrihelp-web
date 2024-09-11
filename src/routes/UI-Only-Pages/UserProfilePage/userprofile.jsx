705b6a63d5539faf89273eaeb5043328e05791c6
Mihirotron <mihirjr2002@gmail.com>
2024-09-11 16:24:29 +1000

    Enhancement

--- src/routes/UI-Only-Pages/UserProfilePage/userprofile.jsx
+++ src/routes/UI-Only-Pages/UserProfilePage/userprofile.jsx
@@ -11,13 +11,13 @@ const UserProfilePage = () => {
   const handleInputValue = (e) => {
     e.preventDefault();
     const userFirstName = firstName.value;
-
+    
     const userLastName = lastName.value;
     const userNumber = number.value;
     const userEmail = email.value;
     const userPassword = password.value;
     const userHealth = health.value;
-
+    
     const userPreferred = preferred.value;
     const inputArray = [
       {
@@ -107,7 +107,7 @@ const UserProfilePage = () => {
               </label>
             </div>
           </div>
-
+         
             <label>
               <input type="checkbox" name="signup"/>
               By Signing up you agree to receive updates and special offers
@@ -124,14 +124,14 @@ const UserProfilePage = () => {
           <input type="file" className="form-control" name="image"/>
         <div className="mt-4 pt-4">
     <label for="Writetous" className="form-label"> Write to Us </label>
-    <textarea className="form-control"  id="comment" rows="4" cols="30"> </textarea>
+    <textarea className="form-control"  id="comment" rows="4" cols="30"> </textarea>   
   </div>
           <button className="signup-facebook mt-4 p-4"> Leave Us a Comment.</button>
-          <button className="signup-help mt-5 p-2"><img src="/images/help.png" class="signupimg me-2"/> Need Help? Talk to Us.</button>
+          <button className="signup-help mt-5 p-2"><img src="/images/h.png" class="signupimg me-2"/> Need Help? Talk to Us.</button>
           </div>
         </div>
     </div>
-    </div>
+    </div>    
   );
 };
 
