0757fe68103f95e98f87f5720fbb78d41a69aeeb
shoaibaliawan <162567223+shoaibaliawan@users.noreply.github.com>
2024-09-22 23:44:43 +1000

    changes in login and signup screen

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../context/user.context';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../context/user.context";
 
 const MainNavbar = () => {
   const { currentUser, logOut } = useContext(UserContext);
@@ -11,34 +11,56 @@ const MainNavbar = () => {
         <div className="nav-links">
           {currentUser ? (
             <>
-              <Link className="link" to='/dashboard'>Menu</Link>
-              <Link className="link" to='/Meal'>Meal Planning</Link>
+              <Link className="link" to="/dashboard">
+                Menu
+              </Link>
+              <Link className="link" to="/Meal">
+                Meal Planning
+              </Link>
 
               <div className="dropdown">
                 <Link className="link">Recipes</Link>
                 <div className="dropdown-content">
-                  <Link className="link" to='/CreateRecipe'>Create Recipe</Link>
-                  <Link className="link" to='/SearchRecipes'>Search Recipes</Link>
+                  <Link className="link" to="/CreateRecipe">
+                    Create Recipe
+                  </Link>
+                  <Link className="link" to="/SearchRecipes">
+                    Search Recipes
+                  </Link>
                 </div>
               </div>
 
               <div className="dropdown">
                 <Link className="link">User</Link>
                 <div className="dropdown-content">
-                  <Link className="link" to='/DietaryRequirements'>Dietary Preference</Link>
-                  <Link className="link" to='/userProfile'>Profile</Link>
+                  <Link className="link" to="/DietaryRequirements">
+                    Dietary Preference
+                  </Link>
+                  <Link className="link" to="/userProfile">
+                    Profile
+                  </Link>
                 </div>
               </div>
 
-              <Link className="link" to='/ScanProducts'>Scan Products</Link>
+              <Link className="link" to="/ScanProducts">
+                Scan Products
+              </Link>
 
-              <button className="link" onClick={logOut}>Log Out</button>
+              <button className="link" onClick={logOut}>
+                Log Out
+              </button>
             </>
           ) : (
             <>
-              <Link className="link" to='/home'>Home</Link>
-              <Link className="link" to='/login'>Sign In</Link>
-              <Link className="link" to='/signUp'>Create Account</Link>
+              <Link className="link" to="/home">
+                Home
+              </Link>
+              <Link className="link" to="/login">
+                Sign In
+              </Link>
+              <Link className="link" to="/signUp">
+                Create Account
+              </Link>
             </>
           )}
         </div>
