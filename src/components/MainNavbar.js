1e6fe1bbf428f1dfed85b7d08faddc74a78ccd5c
GuneeshMehra <mehraguneesh65@gmail.com>
2024-09-24 15:26:58 +1000

    give message name

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,46 +1,55 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../context/user.context';
+import '../styles/mainNavbar.css'; 
 
 const MainNavbar = () => {
   const { currentUser, logOut } = useContext(UserContext);
 
   return (
-    <header className="header">
-      <nav className="nav">
+    <header className="header main-header">
+      <nav className="nav main-nav">
+        <div className="nav-container">
+        <div className="logo-container">
+          
+        <img src="/images/logo.png" alt="Website Logo" className="logo" />
+
+        </div>
+
         <div className="nav-links">
           {currentUser ? (
             <>
-              <Link className="link" to='/dashboard'>Menu</Link>
-              <Link className="link" to='/Meal'>Meal Planning</Link>
+              <Link className="link nav-link" to='/dashboard'>Menu</Link>
+              <Link className="link nav-link" to='/Meal'>Meal Planning</Link>
 
               <div className="dropdown">
-                <Link className="link">Recipes</Link>
+                <Link className="link nav-link">Recipes</Link>
                 <div className="dropdown-content">
-                  <Link className="link" to='/CreateRecipe'>Create Recipe</Link>
-                  <Link className="link" to='/SearchRecipes'>Search Recipes</Link>
+                  <Link className="link dropdown-link" to='/CreateRecipe'>Create Recipe</Link>
+                  <Link className="link dropdown-link" to='/SearchRecipes'>Search Recipes</Link>
                 </div>
               </div>
 
               <div className="dropdown">
-                <Link className="link">User</Link>
+                <Link className="link nav-link">User</Link>
                 <div className="dropdown-content">
-                  <Link className="link" to='/DietaryRequirements'>Dietary Preference</Link>
-                  <Link className="link" to='/userProfile'>Profile</Link>
+                  <Link className="link dropdown-link" to='/DietaryRequirements'>Dietary Preference</Link>
+                  <Link className="link dropdown-link" to='/userProfile'>Profile</Link>
                 </div>
               </div>
 
-              <Link className="link" to='/ScanProducts'>Scan Products</Link>
+              <Link className="link nav-link" to='/ScanProducts'>Scan Products</Link>
 
-              <button className="link" onClick={logOut}>Log Out</button>
+              <button className="link nav-link logout-button" onClick={logOut}>Log Out</button>
             </>
           ) : (
             <>
-              <Link className="link" to='/home'>Home</Link>
-              <Link className="link" to='/login'>Sign In</Link>
-              <Link className="link" to='/signUp'>Create Account</Link>
+              <Link className="link dropdown-link" to='/home'>Home</Link>
+              <Link className="link dropdown-link" to='/login'>Sign In</Link>
+              <Link className="link dropdown-link" to='/signUp'>Create Account</Link>
             </>
           )}
+        </div>
         </div>
       </nav>
     </header>
