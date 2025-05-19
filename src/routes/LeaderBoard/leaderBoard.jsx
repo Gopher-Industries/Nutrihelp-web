fe03003bae897ba6dc461ab8edc4042ea1ed5291
awadhey <awadheybhardwaj@gmail.com>
2025-05-20 06:10:18 +1000

    added

--- src/routes/LeaderBoard/leaderBoard.jsx
+++ src/routes/LeaderBoard/leaderBoard.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // src/routes/Leaderboard/Leaderboard.jsx
 import React, { useState } from "react";
 import FitnessInput from "./components/FitnessInput";
@@ -26,3 +27,33 @@ const Leaderboard = () => {
 };
 
 export default Leaderboard;
+=======
+// src/routes/Leaderboard/Leaderboard.jsx
+import React, { useState } from "react";
+import FitnessInput from "./components/FitnessInput";
+import FitnessJourney from "./components/FitnessJourney";
+import "./leaderBoard.css"; // Make sure this is imported
+
+const Leaderboard = () => {
+  const [hasProfile, setHasProfile] = useState(false); // Simulate backend check
+
+  return (
+    <div className="leaderboard-background">
+      <div className="leaderboard-page">
+        {!hasProfile ? (
+          <FitnessInput onProfileSaved={() => setHasProfile(true)} />
+        ) : (
+          <FitnessJourney/>
+        )}
+        {/* <FitnessJourney 
+          currentWeight={65} // Example weight
+          targetWeight={55}  // Example target weight
+          goalType="Lose weight" // Example goal type
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Leaderboard;
+>>>>>>> Stashed changes
