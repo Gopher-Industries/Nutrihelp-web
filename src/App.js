1f68287618496b134c521d272e0f4566c81a5c68
garyzhang75 <166787037+garyzhang75@users.noreply.github.com>
2025-09-15 22:00:05 +1000

    fix(App.js): clean merge markers and wire /daily-plan-edit

--- src/App.js
+++ src/App.js
@@ -49,16 +49,8 @@ import ObesityPredictor from "./routes/survey/ObesityPredictor";
 import UiTimer from "./routes/UiTimer/UiTimer"
 import Settings from "./routes/Settings/Settings"
 import HealthFAQ from "./routes/HealthFAQ/HealthFAQ";
-<<<<<<< Updated upstream
-import DailyPlanEdit from './routes/DailyPlan/DailyPlanEdit';
 import ScanBarcode from "./routes/ScanBarcode/ScanBarcode";
-=======
->>>>>>> Stashed changes
 import AuthCallback from "./pages/AuthCallback";
-import DailyPlanEdit from "./routes/DailyPlan/DailyPlanEdit";
-
-
-
 import DailyPlanEdit from './routes/DailyPlan/DailyPlanEdit';
 
 function App() {
