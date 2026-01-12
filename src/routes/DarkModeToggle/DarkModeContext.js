b9cf0b7d16da752854e01a0cf744e555b9696be2
DESKTOP-BAB991D/SE <sujaljoshi77@gmail.com>
2026-01-12 16:30:20 +1100

    Dark-mode implemenatation v1

--- src/routes/DarkModeToggle/DarkModeContext.js
+++ src/routes/DarkModeToggle/DarkModeContext.js
@@ -6,16 +6,20 @@ const DarkModeContext = createContext();
 // Context Provider Component
 export const DarkModeProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(
-    localStorage.getItem("darkMode") === "true"
+    localStorage.getItem("darkMode") === "true" || 
+    localStorage.getItem("globalDarkMode") === "true"
   );
 
   useEffect(() => {
     if (darkMode) {
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add("dark-mode");
+      document.body.classList.add("dark-mode");
     } else {
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove("dark-mode");
+      document.body.classList.remove("dark-mode");
     }
     localStorage.setItem("darkMode", darkMode);
+    localStorage.setItem("globalDarkMode", darkMode); // Keep in sync with Settings
   }, [darkMode]);
 
   return (
