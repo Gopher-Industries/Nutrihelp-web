25a29f5c03a332df8bfae13d690bc4cb5d651e4c
James Student <jnardella@deakin.edu.au>
2026-07-17 13:00:38 +1000

    Revert Home TEST back to Home

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -198,7 +198,7 @@ const MainNavbar = () => {
           {/* Left */}
           <div className="nav-left">
             <Link to="/home" className="nav-link">
-              Home TEST
+              Home
             </Link>
 
             <Link to="/scan" className="nav-link nav-link-icon">
